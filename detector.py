263271c178a23ef7efc9d739f0b7b2eb1089609b
Dom <32508991+d00mOfMankind@users.noreply.github.com>
2017-12-21 10:20:39 +0000

    added comments

--- detector.py
+++ detector.py
@@ -1,4 +1,4 @@
-#Dependencies (use pip install [package name])
+#Dependencies (use pip install [package name]), must have pip installed, (apt install python-pip)
 #    gpiozero
 #    picamera
 
@@ -12,8 +12,9 @@ from datetime import datetime #imports date getting function
 print("Importing camera...")
 from picamera import PiCamera #guess what this imports?
 cam = PiCamera()
+#cam.resolution(400, 400) #set resolution of image to take, default is 640x480
 
-import os
+import os #this is used to check if folders exist and create folders
 
 print("Load sequence complete.")
 
@@ -21,26 +22,26 @@ print("Load sequence complete.")
 
 
 def normal():
-	if not (os.path.isdir("vid")):
-		os.makedirs("vid")
+	if not (os.path.isdir("vid")): #if folder does not exist
+		os.makedirs("vid") #create folder
 
-	while True:
-	    pir.wait_for_motion()
+	while True: #loop forever
+	    pir.wait_for_motion() #when PIR detects motion
 	    
-	    now = str(datetime.now())
-	    log = "Motion detected at >" + now
-	    vidLoc = "vid/image_" + now + ".h264"
+	    now = str(datetime.now()) #get datetime and cast to string
+	    log = "Motion detected at >" + now #create log string
+	    vidLoc = "vid/video_" + now + ".h264" #create location to save file (.h264 is MP4)
 	    
-	    print(log)
+	    print(log) #print the log string
 	    
-	    with open("log.txt", "a") as file:
-	        file.write(log + "\n")
-	        file.close()
+	    with open("log.txt", "a") as file: #open the file (will also create if doesn't exist
+	        file.write(log + "\n") #write the log string (and a newline) to the file
+	        file.close() #close file (saves memory and prevents write bugs)
 	        
 	    
-	    cam.start_recording(vidLoc)
-	    pir.wait_for_no_motion()
-	    cam.stop_recording()
+	    cam.start_recording(vidLoc) #start recording
+	    pir.wait_for_no_motion() #wait until there is no motion detected
+	    cam.stop_recording() #stop recording
 
 
 
@@ -64,25 +65,25 @@ def low_fps(): #not finished, DNU
 
 
 
-def static():
-	if not (os.path.isdir("img")):
-		os.makedirs("img")
+def static(): #take static images
+	if not (os.path.isdir("img")): #if folder does not exist
+		os.makedirs("img") #create folder
 
-	while True:
-	    pir.wait_for_motion()
+	while True: #loop forever
+	    pir.wait_for_motion() #wait until motion detected
 	    
-	    now = str(datetime.now())
-	    log = "Motion detected at >" + now
-	    imgLoc = "img/image_" + now + ".png"
+	    now = str(datetime.now()) #get current datetime
+	    log = "Motion detected at >" + now #create log string
+	    imgLoc = "img/image_" + now + ".png" #create location to save image
 	    
-	    print(log)
+	    print(log) #print log test to screen
 	    
-	    with open("log.txt", "a") as file:
-	        file.write(log + "\n")
-	        file.close()
+	    with open("log.txt", "a") as file: #open log file
+	        file.write(log + "\n") #write log string (and newline)
+	        file.close() #close file, prevents errors and is generally just good practice
 
 
-	    cam.capture(imgLoc)
+	    cam.capture(imgLoc) #take a image, save it at location we generate earlier
 
 
 #uncomment what one you want
